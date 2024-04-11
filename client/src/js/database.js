1de57f4f627ec60afbd04ab723833ef4a788654e
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-04-10 20:39:02 -0400

    BIG COMMIT: Full disclosure, I forgot to commit as I was working through the TODOS and by the time I realized, I was at this point of the project: Rundown of commits, added webpack configuration, install button logic, idb get and put logic, imported webplugins. Currently testing application before final push.

---
+++ client/src/js/database.js
@@ -0,0 +1,49 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.add(content);
+
+  const result = await request;
+
+  console.log('Content added to database', result);
+};
+
+// Method that gets all the content from the database
+export const getDb = async () => {
+  
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('Content fetched from database', result);
+
+  return result;
+};
+
+initdb();
---
+++ client/webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Take Notes Netizens'}),
+      
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true, 
+        name: 'Take Notes Netizens',
+        short_name: 'Take Notes',
+        description: 'An app for taking notes',
+        background_color: '#01579b',
+        theme_color: '#01579b',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ]}),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js',
+      }),
+
+      ] ,
+
+    module: {
+      // CSS loaders and Babel configuration
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          //use babel-loader to transpile JavaScript files
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            },
+          },
+        }
+        
+      ],
+    },
+  };
+};
